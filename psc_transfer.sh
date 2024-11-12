41c9e999a0d9ac0234815f8d9166c5149771d76e
Shane McKeon <mckeons@br014.ib.bridges2.psc.edu>
2024-11-12 16:30:31 -0500

    psc files

---
+++ psc_transfer.sh
@@ -0,0 +1,3 @@
+exit 0
+rsync -n -a -v -i -h /Volumes/Hera/Projects/7TBrainMech/scripts/eeg/Shane/preprocessed_data/MGS/AfterWhole/ICAwholeClean_homogenize mckeons@bridges2.psc.edu:~/shared/SignalComplexityAcrossAdolescene/data
+
